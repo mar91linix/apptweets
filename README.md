a39c79b698ddb74e8b099b2a5728f12c4e6250cb
unknown <lzapataalianzaestudainti@gmail.comgit comit mi primer comi>
2020-09-11 11:09:44 -0600

    modificado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# apptweets
+# Tweet App
+
+Una app para almacenar Tweets personales en el localStorage
+
+
+
+## Recurso gratuito
+---
+Desarrollado con :heart: por [Mar](https://github.com/mar91linix/apptweets) :muscle: :blush:
+
--- app.js/app.js
+++ app.js/app.js
@@ -1,100 +1,109 @@
-//variables
-const furmulario = document.querySelector('#formulario');
+// Variables
+const formulario = document.querySelector('#formulario');
 const listadoTweets = document.querySelector('#listado');
 const contentError = document.querySelector('#mensajes');
-let tweet =  [];
+let tweets  = [];
 
-//evenlisten
+// Event Listeners
 eventListeners();
 
- function eventListeners(){
-     //cuando el usuario agrega un nuevo tweet
-     formulario.addEventListener('submit', agregarTweet );
-
-     //cuando carga el DOM
-     document.addEventListener('DOMContentLoaded', () =>{
-        tweets = JSON.parse(localStorage.getItem('tweets') ) || [];
+function eventListeners() {
+    // Cuando el usuario agrega un nuevo tweet
+    formulario.addEventListener('submit', agregarTweet );
+    
+    // Cuando carga el DOM
+    document.addEventListener('DOMContentLoaded', () => {
+        tweets = JSON.parse( localStorage.getItem('tweets') ) || [];
+        // console.log(tweets);
         mostrarHtmlList();
-     }) ;
- }
+    });
+}
 
-//funciones
+// Funciones
 function agregarTweet(e) {
-    e.preventDefaut();
+    e.preventDefault();
 
-    //acceder al textarea del formulario para almacenar lo que escriban en el 
-    const tweet = document.querySelector('#tweet').Value;
+    // Acceder al textarea del formulario para almacenar lo que escriban en el
+    const tweet = document.querySelector('#tweet').value;
 
-     //validar que no venga vacio o con un espacio en blanco el tweet 
-    if(tweet === "" || tweet === "") {
-           mostrarError('ERROR :EL tweet no puede estar vacio');
-           return;
-        }
-        //crear un objeto para almacenar el tweet
-        const tweetObj = {
-            id: Date.now(),
-            tweet
-        };
-    //Anñadir a nuestro areglo de tweets
+    // Validar que no venga vacío o con un espacio en blanco el tweet
+    if (tweet === "" || tweet === " ") {
+        mostrarError('ERROR: El tweet no puede estar vacío!');
+        return;
+    }
 
+    // Crear un objeto para almacenar el tweet
+    const tweetObj  = {
+        id: Date.now(),
+        tweet
+    };
+
+    // Añadir a nuestro arreglo de tweets
     tweets = [...tweets, tweetObj];
 
-      mostrarHtmlList();   
-      //limpiar formulario
-      formulario.reset();
-    };
-    function mostrarError(error) {
+    mostrarHtmlList();
+
+    // Limpiar formulario
+    formulario.reset();
+
+}
+
+
+function mostrarError(error) {
 
     let existeError = contentError.hasChildNodes();
 
-        if (!existError){
-            
+    if (!existeError) {
         const msg = document.createElement('p');
         msg.textContent = error;
         msg.classList.add('alert');
         msg.classList.add('alert-danger');
-
+        
         contentError.appendChild(msg);
-           
-        setTimeout ( () => {
+
+        // Remover el mensaje de error
+        setTimeout( () => {
             msg.remove();
-        },300);
+        }, 3000);
     }
+
 }
 
-function mostrarHtmlList(){
-    // limpiar listado antes de mostarlo 
+function mostrarHtmlList() {
+
+    // Limpiar listado antes de mostrarlo
     limpiarListado();
 
-if (tweets.length > 0) {
-    tweets.forEach( tweetObj => {
-         //crear un boton papa eliminar
-        const btnEliminar = documnt.createElement('i  ');
-        btnEliminar.classList.add('fa');
-        btnEliminar.classList.add('fa-trash');
-        btnEliminar.classList.add('btnEliminar');
-    
-        //crear funcionalidad para eliminzzr tweet
-        btnEliminar.onclik =() => {
-            eliminarTweet(tweetObj.id);
-        }
+    if (tweets.length > 0) {
+        tweets.forEach( tweetObj => {
+            // Creamos un boton para eliminación
+            const btnEliminar = document.createElement('i');
+            btnEliminar.classList.add('fa');
+            btnEliminar.classList.add('fa-trash');
+            btnEliminar.classList.add('btnEliminar');
 
-       // Crear nuevo elemento
-        const li = document.createElement('li');
-        li.innerHTML = tweetObj.tweet;
+            // Crear funcionalidad para eliminar tweet
+            btnEliminar.onclick = () => {
+                eliminarTweet(tweetObj.id);
+            }
 
-        li.appendChild(btnEliminar);
+            // Crear el nuevo elemento
+            const li = document.createElement('li');
+            li.innerHTML = tweetObj.tweet;
 
-        listadoTweets.appendChild(li);
-    });
+            li.appendChild(btnEliminar);
+
+            listadoTweets.appendChild(li);
+        });
+    }
+
+    sincronizarStorage();
 }
-sincronizarStorage();
-}
-// agregar los tweets al storage
 
+// Agregar los tweets al storage
 function sincronizarStorage() {
-     localStorage.setItem('tweets',JSON.stringify(tweets) );
-} 
+    localStorage.setItem('tweets', JSON.stringify(tweets) );
+}
 
 // Limpiamos el listado antes de volverlo a imprimir
 function limpiarListado() {
@@ -102,8 +111,9 @@ function limpiarListado() {
         listadoTweets.removeChild(listadoTweets.firstChild);
     }
 }
- function  eliminarTweet(id) {
-     tweets = tweets.filter(tweet => tweet.id !== id);
 
-     mostrarHtmlList();
- }
+function eliminarTweet(id) {
+    tweets = tweets.filter(tweet => tweet.id !== id);
+    
+    mostrarHtmlList();
+}
